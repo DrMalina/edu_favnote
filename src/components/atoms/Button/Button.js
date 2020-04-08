58005b547af6784adc25ea92d4c2db2d544c83ac
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-04-09 00:37:08 +0200

    Implement Atomic Design, create btn stories

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -1,20 +1,20 @@
 import styled, { css } from 'styled-components';
 
 const Button = styled.button`
-  background-color: #ffd82b;
+  background-color: ${({ color }) => color || 'hsl(49, 100%, 58%)'};
   width: 220px;
   height: 47px;
   border: none;
   border-radius: 50px;
   font-family: 'Montserrat';
-  font-weight: 500;
+  font-weight: 600;
   font-size: 16px;
   text-transform: uppercase;
 
   ${({ secondary }) =>
     secondary &&
     css`
-      background-color: #e6e6e6;
+      background-color: hsl(0, 0%, 90%);
       width: 105px;
       height: 30px;
       font-size: 10px;
---
+++ src/components/atoms/Button/Button.stories.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { withKnobs, select } from '@storybook/addon-knobs';
+import Button from './Button';
+
+export default {
+  component: Button,
+  title: 'Button',
+  decorators: [withKnobs],
+};
+
+export const primary = () => {
+  const label = 'Colors';
+  const options = {
+    Primary: 'hsl(49, 100%, 58%)',
+    Secondary: 'hsl(196, 83%, 75%)',
+    Tertiary: 'hsl(106, 47%, 64%)',
+  };
+  const defaultValue = 'hsl(49, 100%, 58%)';
+  const groupId = 'GROUP-ID1';
+
+  const value = select(label, options, defaultValue, groupId);
+
+  return <Button color={value}>Hello Button</Button>;
+};
+export const secondary = () => <Button secondary>Hello Button</Button>;
