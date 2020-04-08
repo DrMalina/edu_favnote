95854e9aa86eefb413a0091e3207da960192a252
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-04-09 00:52:53 +0200

    Add main theme with sc

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import Button from 'components/atoms/Button/Button';
 import GlobalStyle from 'theme/GlobalStyle';
+import { theme } from 'theme/mainTheme';
 
 const Root = () => (
   <div>
     <GlobalStyle />
-    <h1>Hello There</h1>
-    <Button>Close / Save</Button>
-    <Button secondary>Remove</Button>
+    <ThemeProvider theme={theme}>
+      <>
+        <h1>Hello There</h1>
+        <Button>Close / Save</Button>
+        <Button secondary>Remove</Button>
+      </>
+    </ThemeProvider>
   </div>
 );
 
