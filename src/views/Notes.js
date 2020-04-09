1d81f098d1386f77f21fe02bcb076ae49e0a4d5f
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-04-09 21:32:23 +0200

    Create Note Templates for each view

--- src/views/Notes.js
+++ src/views/Notes.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import UserPageTemplate from 'templates/UserPageTemplate';
+import Card from 'components/molecules/Card/Card';
+
+const notes = [
+  {
+    title: 'Wake me up when Vue ends',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '1 day',
+  },
+  {
+    title: 'Como es An Gular?',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '1 day',
+  },
+  {
+    title: 'Du bist Reactish',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '5 days',
+  },
+  {
+    title: 'Reactuj się kto moze!',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '10 days',
+  },
+];
 
 const Notes = () => (
   <UserPageTemplate pageType="note">
-    <h1>Notes view</h1>
+    {notes.map((item) => (
+      <Card
+        cardType="note"
+        title={item.title}
+        content={item.content}
+        created={item.created}
+        key={item.title}
+      />
+    ))}
   </UserPageTemplate>
 );
 
--- src/views/Tweets.js
+++ src/views/Tweets.js
@@ -1,9 +1,50 @@
 import React from 'react';
 import UserPageTemplate from 'templates/UserPageTemplate';
+import Card from 'components/molecules/Card/Card';
+
+const tweets = [
+  {
+    title: 'Hello Roman',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '1 day',
+    twitterName: 'hello_roman',
+  },
+  {
+    title: 'Redux guy',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '1 day',
+    twitterName: 'dan_abramov',
+  },
+  {
+    title: 'React router stuff',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '5 days',
+    twitterName: 'mjackson',
+  },
+  {
+    title: 'Super animacje!',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
+    created: '10 days',
+    twitterName: 'sarah_edo',
+  },
+];
 
 const Tweets = () => (
   <UserPageTemplate pageType="twitter">
-    <h1>Tweets view</h1>
+    {tweets.map((item) => (
+      <Card
+        cardType="twitter"
+        title={item.title}
+        twitterName={item.twitterName}
+        content={item.content}
+        created={item.created}
+        key={item.title}
+      />
+    ))}
   </UserPageTemplate>
 );
 
